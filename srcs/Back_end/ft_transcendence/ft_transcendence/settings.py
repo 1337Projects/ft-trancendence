3baed53147662ebc1acd7fe072ac7582979bf639
mel-harc <jj>
2024-07-31 17:58:59 +0100

    fix all

--- srcs/Back_end/ft_transcendence/ft_transcendence/settings.py
+++ srcs/Back_end/ft_transcendence/ft_transcendence/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,10 @@ DATABASES = {
         'NAME': 'ft_transcendence',
         'USER': 'ipman',
         'PASSWORD': '133742',
-        'HOST': 'db',
+        'HOST': '172.20.0.3',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
