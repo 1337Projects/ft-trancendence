de556dbfa719d1ddbfa15920038814ec82499d31
mel-harc <jj>
2024-08-05 01:55:32 +0100

    [mel-harc] small test of api (create folder api, modification in seetings project)

--- srcs/Back_end/setup.sh
+++ srcs/Back_end/setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-. virtualenv/bin/activate
+source virtualenv/bin/activate
+
+pip install --upgrade pip
+
+pip install --no-cache-dir -r requirements.txt
+
+pip install djangorestframework
 
 python3 /app/ft_transcendence/manage.py makemigrations; \
 python3 /app/ft_transcendence/manage.py migrate
-
 
 python3 /app/ft_transcendence/manage.py runserver 0.0.0.0:8000
