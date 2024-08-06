6993f2396dd470bb07d58b3cdbf86e0ed8832848
mel-harc <elharchimohammed8@gmail.com>
2024-08-06 16:11:23 +0100

    sum adding

--- srcs/Back_end/ft_transcendence/login/models.py
+++ srcs/Back_end/ft_transcendence/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=10)
+    firstname = models.CharField(max_length=10)
+    lastname = models.CharField(max_length=10)
+    phonenumber = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = "User"
