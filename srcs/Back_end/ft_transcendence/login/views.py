3599ee62a7d97822dfd8409d83b07169cc074440
mel-harc <jj>
2024-08-16 01:24:29 +0100

    [mel-harc] update views.py

--- srcs/Back_end/ft_transcendence/login/views.py
+++ srcs/Back_end/ft_transcendence/login/views.py
@@ -8,30 +8,33 @@ import urllib.parse
 
 
 
-
 def google_oauth(request):
-    # Define the Google OAuth 2.0 endpoint
-    google_oauth_endpoint = "https://accounts.google.com/o/oauth2/auth"
+    if request.method == 'GET':
+        # Define the Google OAuth 2.0 endpoint
+        google_oauth_endpoint = "https://accounts.google.com/o/oauth2/auth"
 
-    # Client ID and Redirect URI from Google Developer Console
-    client_id = "1063752047067-68ak54eimd9bkcorgsvmp918vk85e57c.apps.googleusercontent.com"
-    redirect_uri = "http://localhost:5173/"  # This is where Google will redirect after authentication
-    response_type = "code"  # We're using the authorization code flow
-    scope = "openid email profile"  # Define the scopes you want
-    state = "random_state_string"  # Optional: helps protect against CSRF attacks
+        # Client ID and Redirect URI from Google Developer Console
+        client_id = "1063752047067-68ak54eimd9bkcorgsvmp918vk85e57c.apps.googleusercontent.com"
+        redirect_uri = "http://localhost:5173/"  # This is where Google will redirect after authentication
+        response_type = "code"  # We're using the authorization code flow
+        scope = "openid email profile"  # Define the scopes you want
+        state = "random_state_string"  # Optional: helps protect against CSRF attacks
 
-    # Construct the full URL for the authorization request
-    params = {
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
-        "response_type": response_type,
-        "scope": scope,
-        "state": state,
-        "access_type": "offline",  # Optional: to get a refresh token
-    }
-    
-    # Build the query string
-    oauth_url = f"{google_oauth_endpoint}?{urllib.parse.urlencode(params)}"
+        # Construct the full URL for the authorization request
+        params = {
+            "client_id": client_id,
+            "redirect_uri": redirect_uri,
+            "response_type": response_type,
+            "scope": scope,
+            "state": state,
+            "access_type": "offline",  # Optional: to get a refresh token
+        }
+        
+        # Build the query string
+        oauth_url = f"{google_oauth_endpoint}?{urllib.parse.urlencode(params)}"
 
-    # Return the URL to the frontend
-    return JsonResponse({"url": oauth_url})
+        # Return the URL to the frontend
+        return JsonResponse({"url": oauth_url})
+    elif request.method == 'POST' :
+        return JsonResponse({"code": request.POST.get("code")})
+
