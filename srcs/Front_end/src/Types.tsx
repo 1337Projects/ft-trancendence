2aa575034a4a299b5d2f07ce2273f44fc67ae2b8
Abdelhadi Amhamdi <aamhamdi@e2r3p5.1337.ma>
2024-10-02 12:24:32 +0100

    [add] types

---
+++ srcs/Front_end/src/Types.tsx
@@ -0,0 +1,45 @@
+
+
+export type ApearanceContextType = {
+    theme : string,
+    color : string,
+    themeHandler : (theme : string) => void,
+    colorHandler : (color : string) => void,
+}
+
+export type AuthInfosType = {
+    accessToken : string,
+    username : string,
+}
+
+type UserProfileType = {
+    image : string,
+    bio : string,
+    level : number,
+    rank : number,
+    banner : string
+}
+
+export type UserType = {
+    username : string,
+    first_name : string,
+    last_name : string,
+    profile : UserProfileType
+}
+
+export type FirendType = {
+    id : Number,
+    sender : UserType,
+    receiver : UserType,
+    status : string
+}
+
+export type UserContextType = {
+    authInfos : AuthInfosType | null,
+    user : UserType | null,
+    friends : FirendType[] | null,
+    setAuthInfosHandler : (token : string | null) => void,
+    setUser : React.Dispatch<React.SetStateAction<UserType | null>>
+    setFriends : React.Dispatch<React.SetStateAction<FirendType[] | null>>
+
+}
