6cbcf1720fe8e08cb505c3bd053a632135a287fd
Abdelhadi Amhamdi <aamhamdi943@gmail.com>
2024-11-04 15:00:46 +0100

    [add] tournment consumer and tournment builder class and player and tournment serializers

---
+++ srcs/Back_end/ft_transcendence/tournment/serializers.py
@@ -0,0 +1,26 @@
+
+from rest_framework import serializers
+from .models import Tournment, Player
+from login.models import User
+from login.serializer import UserSerializer
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ["id", "name", "user_id"]
+
+class TournmentSerializer(serializers.ModelSerializer):
+
+    queryset = User.objects.all()
+    owner = serializers.PrimaryKeyRelatedField(queryset=queryset)
+
+    def to_representation(self, instance):
+        representation =  super().to_representation(instance)
+        representation['players'] = PlayerSerializer(instance.players.all(), many=True).data
+        representation['owner'] =  UserSerializer(instance.owner).data
+        return representation
+
+    class Meta:
+        model = Tournment
+        fields = ["id", "mode", "max_players", "owner"]
