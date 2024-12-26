fdda68c29e1d0228a4553f0f1460f80eea3ad9c5
Khawla Ben hammou <kben-ham@e2r1p4.1337.ma>
2024-12-26 01:43:08 +0100

    <kben-ham> add the new CORS_ALLOWED_ORIGINS: https://localhost:1024

--- srcs/Back_end/ft_transcendence/ft_transcendence/settings.py
+++ srcs/Back_end/ft_transcendence/ft_transcendence/settings.py
@@ -78,7 +78,8 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5173",#wa9Ila khas yt7iyd
+    "https://localhost:1024",
 ]
 
 ROOT_URLCONF = 'ft_transcendence.urls'
