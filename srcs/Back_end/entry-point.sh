30139ba6eae6638f1fdc5df87bcf06fa402b6a2c
Khawla Ben hammou <kben-ham@e2r8p3.1337.ma>
2025-01-27 14:33:14 +0100

    <kben-ham> clean entry-point

--- srcs/Back_end/entry-point.sh
+++ srcs/Back_end/entry-point.sh
@@ -2,7 +2,7 @@
 
 /usr/local/bin/wait-for-it.sh database:5432 --timeout=50 --strict -- echo "database is ready!"
 
-python3 /app/manage.py makemigrations login account chat tournment notifications game background_task emojiesProxy
+python3 /app/manage.py makemigrations login account chat tournment notifications game background_task
 python3 /app/manage.py migrate --noinput
 nohup python3 /app/manage.py process_tasks &
 exec "$@"
